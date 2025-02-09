e1d8b5e396090806788328f3b00c8bb0af4e39c3
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-09 17:35:20 +0700

    config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", "7684599268:AAFf97fALKymo_Xi1hb3t0kiFP8Wmis1k8I"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    APP_ID = int(os.environ.get("APP_ID", "26775695"))
+    API_HASH = os.environ.get("API_HASH", "b15bb60859bef151762fc5d9eb206c67")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7684599268:AAFf97fALKymo_Xi1hb3t0kiFP8Wmis1k8I")
     LOGS_CHANNEL = (
-        int(os.environ.get("LOGS_CHANNEL"))
+        int(os.environ.get("LOGS_CHANNEL", "-1002482663527"))
         if os.environ.get("LOGS_CHANNEL").strip("-").isdigit()
-        else os.environ.get("LOGS_CHANNEL")
+        else os.environ.get("LOGS_CHANNEL", "-1002482663527")
     )
-    MONGODB_URL = os.environ.get("MONGODB_URL")
+    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://pangphu9:0pSRO3UHIoH5ouAx@cluster0.ipqp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", "Unzipper_Bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1200411908"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
